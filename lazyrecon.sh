cec7507410b4f4d9fdeae67333aaa02e9e9f6a4e
Filip Ochnik <filip.ochnik@gmail.com>
2020-07-30 09:59:40 +0200

    second dns validation

--- lazyrecon.sh
+++ lazyrecon.sh
@@ -2,7 +2,6 @@
 
 massdnsWordlist=$HOME/tools/SecLists/Discovery/DNS/clean-jhaddix-dns.txt
 massdnsResolvers1="$HOME/tools/massdns/lists/resolvers.txt"
-massdnsResolvers2="$HOME/tools/resolvers2.txt"
 outputDirectory="./lazyrecon_results"
 outputFolder=
 resultsFolder=
@@ -76,7 +75,7 @@ generate_candidates() {
     touch "$outputFolder/candidates.txt"
 
     log "Finding domains using Project Sonar data"
-    curl -s "https://dns.bufferover.run/dns?q=$domain" 2>/dev/null | jq -r '.FDNS_A,.RDNS | .[]?' | sed 's/\*\.//g' | cut -d ',' -f2 | grep -F ".$domain" | sort -u >>"$outputFolder/sonar.txt"
+    curl -s "https://dns.bufferover.run/dns?q=$domain" 2>/dev/null | jq -r '.FDNS_A,.RDNS | .[]?' | sed 's/\*\.//g' | cut -d ',' -f2 | grep -F ".$domain" >"$outputFolder/sonar.txt"
     add_candidates "sonar.txt"
 
     log "Finding subdomains using Amass"
@@ -92,21 +91,28 @@ generate_candidates() {
     add_candidates "subbrute.txt"
 }
 
+extract_domains_from_massdns() {
+    massdnsOutput=$1
+    dst=$2
+    cat "$massdnsOutput" | cut -d' ' -f1 | grep -v "[*]" | sed -e 's/\.$//' | sponge "$dst"
+    sort_unique "$dst"
+}
+
 verify() {
     log "Starting verification"
 
     log "Starting MassDNS subdomain discovery"
-    cat "$outputFolder/candidates.txt" | $HOME/tools/massdns/bin/massdns -r "$HOME/tools/massdns/lists/resolvers.txt" -t A -q -o S >"$outputFolder/mass1.txt"
-    cat "$outputFolder/mass1.txt" | cut -d' ' -f1 | grep -v '*' | sed -e 's/\.$//' | $HOME/tools/massdns/bin/massdns -r "$HOME/tools/resolvers2.txt" -t A -q -o S >"$outputFolder/mass1.txt"
+    cat "$outputFolder/candidates.txt" | $HOME/tools/massdns/bin/massdns -r "$massdnsResolvers1" -t A -q -o S >"$outputFolder/mass1.txt"
+
+    log "Verifying found domains with reliable resolvers"
+    extract_domains_from_massdns "$outputFolder/mass1.txt" "$outputFolder/mass1.txt"
+    printf "1.1.1.1\n8.8.8.8\n9.9.9.9\n" > resolvers2.txt
+    cat "$outputFolder/mass1.txt" | $HOME/tools/massdns/bin/massdns -r "resolvers2.txt" -t A -q -o S >"$outputFolder/mass.txt"
 
     log "Extracting CNAMEs"
     cat "$outputFolder/mass.txt" | grep CNAME >>"$outputFolder/cnames.txt"
 
-    cat "$outputFolder/mass.txt" | awk '{print $1}' | while read -r line; do
-        x="$line"
-        echo "${x%?}" >>"$outputFolder/domains.txt"
-    done
-    sort_unique "$outputFolder/domains.txt"
+    extract_domains_from_massdns "$outputFolder/mass.txt" "$outputFolder/domains.txt"
 }
 
 logo() {
